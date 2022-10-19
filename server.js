2beac7929eea609f68c42d8d703701106f0eefdc
Kristof Kalai <kristof.kalai@supercharge.io>
2022-10-20 01:17:11 +0200

    update

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/sportbuddyweb'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/sportbuddyweb/index.html'));
+app.use(express.static(__dirname + '/dist'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
