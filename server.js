ae5577d3cf488a02f730ef9558348013fc7a9cf5
Kristof Kalai <kristof.kalai@supercharge.io>
2022-10-20 01:06:45 +0200

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/sportbuddyweb'));
+app.use(express.static(__dirname + './dist/sportbuddyweb'));
 app.listen(process.env.PORT || 8080);
