c80691e33cbf08d5f80197aa14660f6b5c7320a8
Kristof Kalai <kristof.kalai@supercharge.io>
2022-10-22 20:58:45 +0200

    update

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -16,8 +16,4 @@ describe('LoginComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
